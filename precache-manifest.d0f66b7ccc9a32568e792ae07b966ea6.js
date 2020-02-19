b1c7dc2c3c621f93edfd1a4e295c6d4b3a19c073
Yoad Gantz <yoad08@gmail.com>
2020-02-19 13:04:07 +0200

    Updates

---
+++ precache-manifest.d0f66b7ccc9a32568e792ae07b966ea6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb7489c3294b5eb7019f9b3f06b5079",
+    "url": "/pixel-perfect/index.html"
+  },
+  {
+    "revision": "0a9364a78bedc527335c",
+    "url": "/pixel-perfect/static/css/main.acbcdc70.chunk.css"
+  },
+  {
+    "revision": "1e8895e36748616f6440",
+    "url": "/pixel-perfect/static/js/2.80930de6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pixel-perfect/static/js/2.80930de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9364a78bedc527335c",
+    "url": "/pixel-perfect/static/js/main.e949895f.chunk.js"
+  },
+  {
+    "revision": "1651f817fcd3f90365d7",
+    "url": "/pixel-perfect/static/js/runtime-main.837625e7.js"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/pixel-perfect/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/pixel-perfect/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "df4410aead66f0adfa80154f254dc562",
+    "url": "/pixel-perfect/static/media/icon-hamburger.df4410ae.svg"
+  }
+]);
